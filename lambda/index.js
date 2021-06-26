d89e5a9a41921399a61887853294672271707eb4
korbyvk <83457358+korbyvk@users.noreply.github.com>
2021-06-26 20:35:50 +0200

    m

--- lambda/index.js
+++ lambda/index.js
@@ -37,7 +37,7 @@ const GetNewFactHandler = {
     const speakOutput = requestAttributes.t('GET_FACT_MESSAGE') + randomFact;
 
     return handlerInput.responseBuilder
-      .speak(speakOutput)
+      .speak('probando 123 probando')
       // Uncomment the next line if you want to keep the session open so you can
       // ask for another fact without first re-opening the skill
       // .reprompt(requestAttributes.t('HELP_REPROMPT'))
@@ -55,7 +55,7 @@ const HelpHandler = {
   handle(handlerInput) {
     const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
     return handlerInput.responseBuilder
-      .speak('bien')
+      .speak(requestAttributes.t('HELP_MESSAGE'))
       .reprompt(requestAttributes.t('HELP_REPROMPT'))
       .getResponse();
   },
